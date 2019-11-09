f03baa3e7289199206fdba2724c8d0ba88dc9202
leon <leon@qq.com>
2019-11-09 15:45:54 +0800

    first view

--- manage_systerm/myblog/views.py
+++ manage_systerm/myblog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+
+def index(request):
+    """
+    首页访问
+    :return:
+    """
+    return HttpResponse("perfect")
